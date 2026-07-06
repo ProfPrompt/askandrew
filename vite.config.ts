541bffe0dbd39fbf23568d2634dd45f54fc085d5
Andrew Watts <101219978+ProfPrompt@users.noreply.github.com>
2026-07-06 21:17:07 +0100

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/',
+    base: '/askandrew/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
